a0038c9b959b49d67677e2cfc169e2a008d78226
rujal408 <rujal408@gmail.com>
2025-11-09 09:30:14 +0545

    create border for resizing

--- packages/react-virtual/lib/table/th.tsx
+++ packages/react-virtual/lib/table/th.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useTheadContext } from "./thead";
 
 interface IProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -13,29 +13,49 @@ const Th = ({ children, style, colIndex, width = 100, ...props }: IProps) => {
   // colIndex is injected by Thead component via React.cloneElement
   const effectiveColIndex = colIndex ?? 0;
 
+  const ref = useRef<HTMLDivElement>(null);
+
+  const showRightBorder =
+    columnCount > 0 && effectiveColIndex < columnCount - 1;
+
   return (
     <div
+      ref={ref}
       style={{
         width: columnWidths[effectiveColIndex],
-        borderRight:
-          columnCount > 0 && effectiveColIndex < columnCount - 1
-            ? "1px solid #e0e0e0"
-            : "none",
+        borderRight: showRightBorder ? `1px solid #e0e0e0` : "none",
         display: "flex",
         alignItems: "center",
         userSelect: "none",
         flexShrink: 0,
         flexGrow: 0,
         boxSizing: "border-box",
-        ...style,
         textAlign: "left",
         padding: "8px 16px",
         fontWeight: "600",
         fontSize: "14px",
+        position: "relative",
+        ...style,
       }}
       {...props}
     >
       {children}
+
+      {/* 👇 Invisible hit zone for right border hover detection */}
+      {/* {showRightBorder && (
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: -5,
+            width: "10px", // hover zone width (adjust as needed)
+            height: "100%",
+            cursor: "col-resize", // show resize cursor
+            zIndex: 10,
+          }}
+          
+        />
+      )} */}
     </div>
   );
 };
